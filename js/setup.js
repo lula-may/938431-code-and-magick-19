38854bb977430d077635c5a4dd0bf9109b74502e
Yulia Elagina <julia-elagina@yandex.ru>
2020-01-27 20:23:21 +0300

    Добавляет похожих магов в диалоговое окно

---
+++ js/setup.js
@@ -0,0 +1,51 @@
+'use strict';
+
+var userDialog = document.querySelector('.setup');
+userDialog.classList.remove('hidden');
+
+var similarListElement = userDialog.querySelector('.setup-similar-list');
+
+var similarWizardTemplate = document.querySelector('#similar-wizard-template')
+  .content
+  .querySelector('.setup-similar-item');
+
+var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+
+var getRandomElement = function (arr) {
+  var randomIndex = Math.floor(Math.random() * arr.length);
+  var element = arr[randomIndex];
+  return element;
+};
+
+var createWizardList = function (names, surnames, coats, eyes) {
+  var arr = [];
+  for (var i = 0; i < 4; i++) {
+    var newElement = {};
+    newElement.name = getRandomElement(names) + ' ' + getRandomElement(surnames);
+    newElement.coatColor = getRandomElement(coats);
+    newElement.eyesColor = getRandomElement(eyes);
+    arr.push(newElement);
+  }
+  return arr;
+};
+
+var renderWizard = function (wizard) {
+  var wizardElement = similarWizardTemplate.cloneNode(true);
+  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+  return wizardElement;
+};
+
+var wizards = createWizardList(WIZARD_NAMES, WIZARD_SURNAMES, COAT_COLORS, EYES_COLORS);
+
+var fragment = document.createDocumentFragment();
+for (var i = 0; i < wizards.length; i++) {
+  fragment.appendChild(renderWizard(wizards[i]));
+}
+
+similarListElement.appendChild(fragment);
+userDialog.querySelector('.setup-similar').classList.remove('hidden');
