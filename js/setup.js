e19610cf13c6ba5ecbe3bdd9394cb97d7361b5b1
Yulia Elagina <julia-elagina@yandex.ru>
2020-01-29 21:22:25 +0300

    Переименовывает некоторые переменные и убирает константы из параметров функций

--- js/setup.js
+++ js/setup.js
@@ -1,36 +1,36 @@
 'use strict';
 
-var userDialog = document.querySelector('.setup');
-userDialog.classList.remove('hidden');
-
-var similarListElement = userDialog.querySelector('.setup-similar-list');
-
-var similarWizardTemplate = document.querySelector('#similar-wizard-template')
-  .content
-  .querySelector('.setup-similar-item');
-
 var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+var WIZARD_QUANTITY = 4;
 
 var getRandomElement = function (arr) {
   var randomIndex = Math.floor(Math.random() * arr.length);
-  var element = arr[randomIndex];
-  return element;
+  return arr[randomIndex];
 };
 
-var createWizardList = function (names, surnames, coats, eyes) {
-  var arr = [];
-  for (var i = 0; i < 4; i++) {
+var createWizardList = function () {
+  var results = [];
+  for (var i = 0; i < WIZARD_QUANTITY; i++) {
     var newElement = {};
-    newElement.name = getRandomElement(names) + ' ' + getRandomElement(surnames);
-    newElement.coatColor = getRandomElement(coats);
-    newElement.eyesColor = getRandomElement(eyes);
-    arr.push(newElement);
+    newElement.name = getRandomElement(WIZARD_NAMES) + ' ' + getRandomElement(WIZARD_SURNAMES);
+    newElement.coatColor = getRandomElement(COAT_COLORS);
+    newElement.eyesColor = getRandomElement(EYES_COLORS);
+    results.push(newElement);
   }
-  return arr;
+  return results;
 };
+
+var userSetupElement = document.querySelector('.setup');
+userSetupElement.classList.remove('hidden');
+
+var similarListElement = userSetupElement.querySelector('.setup-similar-list');
+
+var similarWizardTemplate = document.querySelector('#similar-wizard-template')
+  .content
+  .querySelector('.setup-similar-item');
 
 var renderWizard = function (wizard) {
   var wizardElement = similarWizardTemplate.cloneNode(true);
@@ -40,12 +40,11 @@ var renderWizard = function (wizard) {
   return wizardElement;
 };
 
-var wizards = createWizardList(WIZARD_NAMES, WIZARD_SURNAMES, COAT_COLORS, EYES_COLORS);
-
+var wizards = createWizardList();
 var fragment = document.createDocumentFragment();
 for (var i = 0; i < wizards.length; i++) {
   fragment.appendChild(renderWizard(wizards[i]));
 }
 
 similarListElement.appendChild(fragment);
-userDialog.querySelector('.setup-similar').classList.remove('hidden');
+userSetupElement.querySelector('.setup-similar').classList.remove('hidden');
