3077949cd67f665162afb414f073b0b5e0166003
Yulia Elagina <julia-elagina@yandex.ru>
2020-02-17 22:57:45 +0300

    Добавляет функционал адаптации данных с сервера

--- js/wizards.js
+++ js/wizards.js
@@ -5,6 +5,28 @@
   var similarElement = document.querySelector('.setup-similar');
   var similarList = similarElement.querySelector('.setup-similar-list');
 
+  var convert = {
+    name: 'name',
+    colorCoat: 'coatColor',
+    colorEyes: 'eyesColor',
+    colorFireball: 'fireballColor',
+    artifacts: 'artifacts'
+  };
+
+  var adaptServerData = function (data) {
+    var newData = [];
+    data.forEach(function (el) {
+      var newElement = {};
+      for (var property in el) {
+        if ({}.hasOwnProperty.call(el, property)) {
+          newElement[convert[property]] = el[property];
+        }
+      }
+      newData.push(newElement);
+    });
+    return newData;
+  };
+
   var getWizardsFragment = function (data) {
     var similarWizardTemplate = document.querySelector('#similar-wizard-template')
     .content
@@ -18,14 +40,14 @@
     var renderWizard = function (wizard) {
       var wizardElement = similarWizardTemplate.cloneNode(true);
       wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
-      wizardElement.querySelector('.wizard-coat').style.fill = wizard.colorCoat;
-      wizardElement.querySelector('.wizard-eyes').style.fill = wizard.colorEyes;
+      wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+      wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
       return wizardElement;
     };
 
     var fragment = document.createDocumentFragment();
     for (var i = 0; i < WIZARD_AMOUNT; i++) {
-      var nextRandomWizard = getRandomElement(data);
+      var nextRandomWizard = getRandomElement(adaptServerData(data));
       fragment.appendChild(renderWizard(nextRandomWizard));
     }
 
