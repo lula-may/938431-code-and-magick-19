7b62fefa9d62ef4ab46ca04b4d8ffe3a05f588b4
Yulia Elagina <julia-elagina@yandex.ru>
2020-02-17 16:05:10 +0300

    Переписывает модуль загрузки/выгрузки с сервера

--- js/backend.js
+++ js/backend.js
@@ -6,80 +6,65 @@
     NO_AUTHORIZED_ERROR: 401,
     NOT_FOUND_ERROR: 404
   };
+
+  var Url = {
+    DOWNLOAD: 'https://js.dump.academy/code-and-magick/data',
+    UPLOAD: 'https://js.dump.academy/code-and-magick'
+  };
+
   var TIMEOUT_IN_MS = 10000;
 
-  var load = function (onLoad, onError) {
-    var URL = 'https://js.dump.academy/code-and-magick/data';
-    var xhr = new XMLHttpRequest();
+  var parseResponse = function (request, onSuccess, onError) {
+    switch (request.status) {
+      case Code.STATUS_OK:
+        onSuccess(request.response);
+        break;
+      case Code.REQUEST_ERROR:
+        onError('Ошибка загрузки данных. Неверный запрос к серверу.');
+        break;
+      case Code.NO_AUTHORIZED_ERROR:
+        onError('Пользователь не авторизован. Требуется авторизация.');
+        break;
+      case Code.NOT_FOUND_ERROR:
+        onError('Ошибка загрузки данных. Сервер не найден.');
+        break;
+      default:
+        onError('Ошибка загрузки данных с сервера. Статус ответа: ' + request.status + ' ' + request.statusText);
+    }
+  };
 
-    var loadHandler = function () {
-      switch (xhr.status) {
-        case Code.STATUS_OK:
-          onLoad(xhr.response);
-          break;
-        case Code.REQUEST_ERROR:
-          onError('Ошибка загрузки данных. Неверный запрос к серверу.');
-          break;
-        case Code.NO_AUTHORIZED_ERROR:
-          onError('Пользователь не авторизован. Требуется авторизация.');
-          break;
-        case Code.NOT_FOUND_ERROR:
-          onError('Ошибка загрузки данных. Сервер не найден.');
-          break;
-        default:
-          onError('Ошибка загрузки данных с сервера. Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
-      }
-    };
-
-    xhr.responseType = 'json';
-    xhr.addEventListener('load', loadHandler);
-    xhr.addEventListener('error', function () {
+  var hangUpEventListeners = function (request, onSuccess, onError) {
+    request.addEventListener('load', function () {
+      parseResponse(request, onSuccess, onError);
+    });
+    request.addEventListener('error', function () {
       onError('Произошла ошибка соединения. Проверьте подключение к интернету.');
     });
-    xhr.addEventListener('timeout', function () {
-      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс.');
+    request.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + request.timeout + 'мс.');
     });
+  };
+
+  var download = function (onLoad, onError) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
     xhr.timeout = TIMEOUT_IN_MS;
-    xhr.open('GET', URL);
+    hangUpEventListeners(xhr, onLoad, onError);
+    xhr.open('GET', Url.DOWNLOAD);
     xhr.send();
   };
 
   var upload = function (data, onSuccess, onError) {
-    var URL = 'https://js.dump.academy/code-and-magick';
-
-    var loadHandler = function () {
-      switch (xhr.status) {
-        case Code.STATUS_OK:
-          onSuccess();
-          break;
-        case Code.REQUEST_ERROR:
-          onError('Ошибка загрузки данных. Неверный запрос к серверу.');
-          break;
-        case Code.NO_AUTHORIZED_ERROR:
-          onError('Пользователь не авторизован. Требуется авторизация.');
-          break;
-        case Code.NOT_FOUND_ERROR:
-          onError('Ошибка загрузки данных. Сервер не найден.');
-          break;
-        default:
-          onError('Ошибка загрузки данных. Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
-      }
-    };
     var xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
-    xhr.open('POST', URL);
-    xhr.addEventListener('load', loadHandler);
-    xhr.addEventListener('error', function () {
-      onError('Произошла ошибка соединения. Проверьте подключение к интернету.');
-    });
-    xhr.addEventListener('timeout', function () {
-      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс.');
-    });
+    xhr.timeout = TIMEOUT_IN_MS;
+    hangUpEventListeners(xhr, onSuccess, onError);
+    xhr.open('POST', Url.UPLOAD);
     xhr.send(data);
   };
 
   window.backend = {
-    load: load,
+    load: download,
     save: upload
   };
 })();
